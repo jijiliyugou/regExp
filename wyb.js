91974697d420048a56cee5de6e24e1eac52e271e
吉吉工作室 <597892949@qq.com>
2020-09-15 12:11:26 +0800

    Update wyb.js

--- wyb.js
+++ wyb.js
@@ -337,7 +337,98 @@ class wyb {
       return v;
     });
   }
+  
+  /*
+     * 时间戳显示为多少分钟前，多少天前的处理
+     * console.log(dateDiff(1411111111111));  // 2014年09月19日
+     */
+    dateDiff(time) {
+      let timestamp = Number(new Date(time));
+      let arrTimestamp = (timestamp + "").split("");
+      for (var start = 0; start < 13; start++) {
+        if (!arrTimestamp[start]) {
+          arrTimestamp[start] = "0";
+        }
+      }
+      timestamp = arrTimestamp.join("") * 1;
 
+      var minute = 1000 * 60;
+      var hour = minute * 60;
+      var day = hour * 24;
+      var halfamonth = day * 15;
+      var month = day * 30;
+      var now = new Date().getTime();
+      var diffValue = now - timestamp;
+
+      // 如果本地时间反而小于变量时间
+      if (diffValue < 0) {
+        return "不久前";
+      }
+
+      // 计算差异时间的量级
+      var monthC = diffValue / month;
+      var weekC = diffValue / (7 * day);
+      var dayC = diffValue / day;
+      var hourC = diffValue / hour;
+      var minC = diffValue / minute;
+
+      // 数值补0方法
+      var zero = function(value) {
+        if (value < 10) {
+          return "0" + value;
+        }
+        return value;
+      };
+
+      // 使用
+      if (monthC > 12) {
+        // 超过1年，直接显示年月日
+        return (function() {
+          var date = new Date(timestamp);
+          return (
+            date.getFullYear() +
+            "年" +
+            zero(date.getMonth() + 1) +
+            "月" +
+            zero(date.getDate()) +
+            "日"
+          );
+        })();
+      } else if (monthC >= 1) {
+        return parseInt(monthC) + "月前";
+      } else if (weekC >= 1) {
+        return parseInt(weekC) + "周前";
+      } else if (dayC >= 1) {
+        return parseInt(dayC) + "天前";
+      } else if (hourC >= 1) {
+        return parseInt(hourC) + "小时前";
+      } else if (minC >= 1) {
+        return parseInt(minC) + "分钟前";
+      }
+      return "刚刚";
+    }
+  
+/*
+ *  将base64转换为文件对象
+ */
+    dataURLtoFile(dataurl, filename) {
+      let arr = dataurl.split(",");
+      let mime = arr[0].match(/:(.*?);/)[1];
+      let bstr = atob(arr[1]);
+      let n = bstr.length;
+      let u8arr = new Uint8Array(n);
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+      console.log(mime);
+      // 转换成file对象
+      // return new File([u8arr], filename + "." + mime.split("/")[1], {
+      //   type: mime
+      // });
+      // 转换成成blob对象
+      // return new Blob([u8arr],{type:mime});
+    }
+      
   /**
    * 将日期时间对象转成格式为"2019-01-20 12:20:01"的字符串，默认获取当前时间
    * @param {Date} date 默认当前时间
